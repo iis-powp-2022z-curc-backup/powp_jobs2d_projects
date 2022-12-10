753b9a74b68f0e30c6e07e21b0792b6f56a492a2
Justyna Owczarek <229301@edu.p.lodz.pl>
2022-12-10 13:33:22 +0100

    Add CommandsRecorderDriver

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/CommandsRecorderDriver.java
@@ -0,0 +1,27 @@
+package edu.kis.powp.jobs2d.drivers;
+
+import edu.kis.powp.jobs2d.command.DriverCommand;
+import edu.kis.powp.jobs2d.command.OperateToCommand;
+import edu.kis.powp.jobs2d.command.SetPositionCommand;
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandsRecorderDriver implements Job2dDriver {
+    private final List<DriverCommand> commands = new ArrayList<>();
+
+    @Override
+    public void setPosition(int x, int y) {
+        commands.add(new SetPositionCommand(x, y));
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        commands.add(new OperateToCommand(x, y));
+    }
+
+    public List<DriverCommand> getCommands() {
+        return commands;
+    }
+}
