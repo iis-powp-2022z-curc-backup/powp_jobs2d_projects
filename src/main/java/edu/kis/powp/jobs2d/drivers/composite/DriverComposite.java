cada80657e2839700963b029fdd721e3bb6e3f1f
Czarek <keracz1997@gmail.com>
2022-12-10 14:36:48 +0100

    add driver component implementation

--- src/main/java/edu/kis/powp/jobs2d/drivers/composite/DriverComposite.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/composite/DriverComposite.java
@@ -2,6 +2,17 @@ package edu.kis.powp.jobs2d.drivers.composite;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
 
-public class DriverComposite {
+import java.util.List;
 
+public class DriverComposite implements IDriverComposite {
+
+    private List<IDriverComposite> CommandList;
+    public DriverComposite(List<IDriverComposite> CommandList){
+        this.CommandList = CommandList;
+    }
+
+    @Override
+    public void execute(Job2dDriver driver) {
+        this.CommandList.forEach(driverCommand -> driverCommand.execute(driver));
+    }
 }
