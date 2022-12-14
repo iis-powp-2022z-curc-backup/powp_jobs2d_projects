53300260d86c6d88dccd92df130a7e112f2de2e5
Czarek Karczewski <keracz1997@gmail.com>
2022-12-14 21:14:28 +0100

    change spaces to tabs

--- src/main/java/edu/kis/powp/jobs2d/drivers/composite/DriverComposite.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/composite/DriverComposite.java
@@ -6,25 +6,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DriverComposite implements Job2dDriver{
-    private List<Job2dDriver> DriverLists = new ArrayList<Job2dDriver>();
+	private List<Job2dDriver> DriverLists = new ArrayList<Job2dDriver>();
 
 
-    public void add(Job2dDriver driver){
-        this.DriverLists.add(driver);
-    }
+	public void add(Job2dDriver driver){
+		this.DriverLists.add(driver);
+	}
 
 
-    @Override
-    public void setPosition(int x, int y) {
-        for (Job2dDriver driver : DriverLists){
-            driver.setPosition(x,y);
-        }
-    }
+	@Override
+	public void setPosition(int x, int y) {
+		for (Job2dDriver driver : DriverLists){
+			driver.setPosition(x,y);
+		}
+	}
 
-    @Override
-    public void operateTo(int x, int y) {
-        for (Job2dDriver driver: DriverLists) {
-            driver.operateTo(x,y);
-        }
-    }
+	@Override
+	public void operateTo(int x, int y) {
+		for (Job2dDriver driver: DriverLists) {
+			driver.operateTo(x,y);
+		}
+	}
 }
