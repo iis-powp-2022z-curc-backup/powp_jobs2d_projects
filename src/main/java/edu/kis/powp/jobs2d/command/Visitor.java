efa81d2396dc78757a1a0caa3a30e021d2befc50
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2022-12-16 22:00:31 +0100

    Convert indents to tabs

--- src/main/java/edu/kis/powp/jobs2d/command/Visitor.java
+++ src/main/java/edu/kis/powp/jobs2d/command/Visitor.java
@@ -2,8 +2,8 @@ package edu.kis.powp.jobs2d.command;
 
 public interface Visitor {
 
-    public void visitICompoundCommand(ICompoundCommand iCompoundCommand);
-    public void visitOperateToCommand(OperateToCommand operateToCommand);
-    public void visitSetPositionCommand(SetPositionCommand setPositionCommand);
+	public void visitICompoundCommand(ICompoundCommand iCompoundCommand);
+	public void visitOperateToCommand(OperateToCommand operateToCommand);
+	public void visitSetPositionCommand(SetPositionCommand setPositionCommand);
 
 }
