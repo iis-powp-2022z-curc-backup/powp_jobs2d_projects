d2a0a29bf1a94b75c055ca14f0793518809b934a
kamilmichalskiya <kamilmichalskiya@users.noreply.github.com>
2022-12-17 12:42:17 +0100

    Add builder of ComplexCommand

--- src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
@@ -2,6 +2,7 @@ package edu.kis.powp.jobs2d.command;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -29,6 +30,10 @@ public class ComplexCommand implements ICompoundCommand {
 		this.driverCommandList.add(command);
 	}
 
+	public static ComplexCommandBuilder builder() {
+		return new ComplexCommandBuilder();
+	}
+
 	@Override
 	public void execute(Job2dDriver driver) {
 		driverCommandList.forEach(driverCommand -> driverCommand.execute(driver));
@@ -44,12 +49,31 @@ public class ComplexCommand implements ICompoundCommand {
 		return name;
 	}
 
-    @Override
-    public Object clone() {
-        List<DriverCommand> commandsListToClone = new LinkedList<>();
-        for (DriverCommand driverCommand: driverCommandList) {
-            commandsListToClone.add( (DriverCommand) driverCommand.clone());
-        }
-        return new ComplexCommand(commandsListToClone, this.name);
-    }
+	@Override
+	public Object clone() {
+		List<DriverCommand> commandsListToClone = new LinkedList<>();
+		for (DriverCommand driverCommand : driverCommandList) {
+			commandsListToClone.add((DriverCommand) driverCommand.clone());
+		}
+		return new ComplexCommand(commandsListToClone, this.name);
+	}
+
+	public static class ComplexCommandBuilder {
+		private final List<DriverCommand> commands = new ArrayList<>();
+		private String name;
+
+		public ComplexCommandBuilder addCommand(final DriverCommand command) {
+			this.commands.add(command);
+			return this;
+		}
+
+		public ComplexCommandBuilder setName(final String name) {
+			this.name = name;
+			return this;
+		}
+
+		public ComplexCommand build() {
+			return new ComplexCommand(commands, name);
+		}
+	}
 }
