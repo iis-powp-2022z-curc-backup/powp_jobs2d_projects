ccea31a679a13bb56a555f47b4d8d5675cc6373c
kamilmichalskiya <kamilmichalskiya@users.noreply.github.com>
2022-12-17 13:10:30 +0100

    Replace spaces to tabs

--- src/test/java/edu/kis/powp/jobs2d/events/SelectLoadSecretCommandOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectLoadSecretCommandOptionListener.java
@@ -1,39 +1,37 @@
 package edu.kis.powp.jobs2d.events;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
-
 import edu.kis.powp.jobs2d.command.ComplexCommand;
-import edu.kis.powp.jobs2d.command.DriverCommand;
 import edu.kis.powp.jobs2d.command.OperateToCommand;
 import edu.kis.powp.jobs2d.command.SetPositionCommand;
 import edu.kis.powp.jobs2d.command.manager.DriverCommandManager;
 import edu.kis.powp.jobs2d.features.CommandsFeature;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class SelectLoadSecretCommandOptionListener implements ActionListener {
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        ComplexCommand complexCommand = ComplexCommand.builder().addCommand(new SetPositionCommand(-20, -50))
-                .addCommand(new OperateToCommand(-20, -50))
-                .addCommand(new SetPositionCommand(-20, -40))
-                .addCommand(new OperateToCommand(-20, 50))
-                .addCommand(new SetPositionCommand(0, -50))
-                .addCommand(new OperateToCommand(0, -50))
-                .addCommand(new SetPositionCommand(0, -40))
-                .addCommand(new OperateToCommand(0, 50))
-                .addCommand(new SetPositionCommand(70, -50))
-                .addCommand(new OperateToCommand(20, -50))
-                .addCommand(new OperateToCommand(20, 0))
-                .addCommand(new OperateToCommand(70, 0))
-                .addCommand(new OperateToCommand(70, 50))
-                .addCommand(new OperateToCommand(20, 50))
-                .setName("TopSecretCommand")
-                .build();
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		ComplexCommand complexCommand = ComplexCommand.builder()
+				.addCommand(new SetPositionCommand(-20, -50))
+				.addCommand(new OperateToCommand(-20, -50))
+				.addCommand(new SetPositionCommand(-20, -40))
+				.addCommand(new OperateToCommand(-20, 50))
+				.addCommand(new SetPositionCommand(0, -50))
+				.addCommand(new OperateToCommand(0, -50))
+				.addCommand(new SetPositionCommand(0, -40))
+				.addCommand(new OperateToCommand(0, 50))
+				.addCommand(new SetPositionCommand(70, -50))
+				.addCommand(new OperateToCommand(20, -50))
+				.addCommand(new OperateToCommand(20, 0))
+				.addCommand(new OperateToCommand(70, 0))
+				.addCommand(new OperateToCommand(70, 50))
+				.addCommand(new OperateToCommand(20, 50))
+				.setName("TopSecretCommand")
+				.build();
 
-        DriverCommandManager manager = CommandsFeature.getDriverCommandManager();
-        manager.setCurrentCommand(complexCommand);
-    }
+		DriverCommandManager manager = CommandsFeature.getDriverCommandManager();
+		manager.setCurrentCommand(complexCommand);
+	}
 }
--- src/test/java/edu/kis/powp/jobs2d/events/SelectRectangleCommandOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectRectangleCommandOptionListener.java
@@ -1,7 +1,6 @@
 package edu.kis.powp.jobs2d.events;
 
 import edu.kis.powp.jobs2d.command.ComplexCommand;
-import edu.kis.powp.jobs2d.command.DriverCommand;
 import edu.kis.powp.jobs2d.command.OperateToCommand;
 import edu.kis.powp.jobs2d.command.SetPositionCommand;
 import edu.kis.powp.jobs2d.command.manager.DriverCommandManager;
@@ -9,21 +8,15 @@ import edu.kis.powp.jobs2d.features.CommandsFeature;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SelectRectangleCommandOptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		ComplexCommand complexCommand = ComplexCommand.builder().addCommand(new SetPositionCommand(-100, -100))
-				.addCommand(new OperateToCommand(-100, -100))
-				.addCommand(new OperateToCommand(-100, 100))
-				.addCommand(new OperateToCommand(200, 100))
-				.addCommand(new OperateToCommand(200, -100))
-				.addCommand(new OperateToCommand(-100, -100))
-				.setName("RectangleCommand")
-				.build();
+				.addCommand(new OperateToCommand(-100, -100)).addCommand(new OperateToCommand(-100, 100))
+				.addCommand(new OperateToCommand(200, 100)).addCommand(new OperateToCommand(200, -100))
+				.addCommand(new OperateToCommand(-100, -100)).setName("RectangleCommand").build();
 
 		DriverCommandManager manager = CommandsFeature.getDriverCommandManager();
 		manager.setCurrentCommand(complexCommand);
