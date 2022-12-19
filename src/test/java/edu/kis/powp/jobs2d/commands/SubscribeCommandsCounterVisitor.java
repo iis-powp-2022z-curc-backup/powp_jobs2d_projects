771acd2001cd3cf06824b147dce93ee4c35cd6e0
Bartłomiej Ołubek <bartek.olubek@gmail.com>
2022-12-19 16:36:03 +0100

    Convert spaces to tabs

--- src/test/java/edu/kis/powp/jobs2d/commands/SubscribeCommandsCounterVisitor.java
+++ src/test/java/edu/kis/powp/jobs2d/commands/SubscribeCommandsCounterVisitor.java
@@ -8,20 +8,20 @@ import edu.kis.powp.observer.Subscriber;
 import java.util.logging.Logger;
 
 public class SubscribeCommandsCounterVisitor implements Subscriber {
-    DriverCommandManager manager = CommandsFeature.getDriverCommandManager();
-    Logger logger = Logger.getLogger("global");
-    private CommandsCounterVisitor commandsCounterVisitor;
-        public SubscribeCommandsCounterVisitor(CommandsCounterVisitor commandsCounterVisitor) {
-            this.commandsCounterVisitor = commandsCounterVisitor;
-        }
+	DriverCommandManager manager = CommandsFeature.getDriverCommandManager();
+	Logger logger = Logger.getLogger("global");
+	private CommandsCounterVisitor commandsCounterVisitor;
+		public SubscribeCommandsCounterVisitor(CommandsCounterVisitor commandsCounterVisitor) {
+			this.commandsCounterVisitor = commandsCounterVisitor;
+		}
 
-        @Override
-        public void update() {
-        manager.getCurrentCommand().accept(commandsCounterVisitor);
+		@Override
+		public void update() {
+		manager.getCurrentCommand().accept(commandsCounterVisitor);
 
-        this.logger.info("OperateToCommand wywołano " + commandsCounterVisitor.getOperateToCommandCount()  + " razy");
-        this.logger.info("SetPositionCommand wywołano " + commandsCounterVisitor.getSetPositionCommandCount()  + " razy");
-    }
+		this.logger.info("OperateToCommand wywołano " + commandsCounterVisitor.getOperateToCommandCount()  + " razy");
+		this.logger.info("SetPositionCommand wywołano " + commandsCounterVisitor.getSetPositionCommandCount()  + " razy");
+	}
 
 
 }
