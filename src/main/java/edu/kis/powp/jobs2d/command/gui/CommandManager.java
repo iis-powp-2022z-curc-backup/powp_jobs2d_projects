531402e0287b1f538d916d308cf7301873111aa1
Hubertius <hubert.kopec@interia.pl>
2022-12-28 16:37:53 +0100

    Add of CommandManager interface methods

--- src/main/java/edu/kis/powp/jobs2d/command/gui/CommandManager.java
+++ src/main/java/edu/kis/powp/jobs2d/command/gui/CommandManager.java
@@ -1,2 +1,12 @@
-package edu.kis.powp.jobs2d.command.gui;public interface CommandManager {
+package edu.kis.powp.jobs2d.command.gui;
+
+import edu.kis.powp.jobs2d.command.DriverCommand;
+import edu.kis.powp.observer.Publisher;
+
+public interface CommandManager {
+    void runCommand();
+    void clearCurrentCommand();
+    String getCurrentCommandAsText();
+    void setCurrentCommand(DriverCommand driverCommand);
+    Publisher getChangePublisher();
 }
