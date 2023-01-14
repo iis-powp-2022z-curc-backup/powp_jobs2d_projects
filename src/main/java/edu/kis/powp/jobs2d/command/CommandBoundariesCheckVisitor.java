09e337e35266db7fb7be781241b0c3ff48db1bc5
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-01-14 13:26:17 +0100

    Add dummy CommandBoundariesCheckVisitor methods content

--- src/main/java/edu/kis/powp/jobs2d/command/CommandBoundariesCheckVisitor.java
+++ src/main/java/edu/kis/powp/jobs2d/command/CommandBoundariesCheckVisitor.java
@@ -1,18 +1,32 @@
 package edu.kis.powp.jobs2d.command;
 
+import java.util.Iterator;
+
 public class CommandBoundariesCheckVisitor implements Visitor{
 	@Override
 	public void visitICompoundCommand(ICompoundCommand iCompoundCommand) {
+		Iterator<DriverCommand> iterator = iCompoundCommand.iterator();
 
+		while(iterator.hasNext()) {
+			DriverCommand driverCommand = iterator.next();
+
+			driverCommand.accept(this);
+		}
 	}
 
 	@Override
 	public void visitOperateToCommand(OperateToCommand operateToCommand) {
+		System.out.println("VISIT OPERATE TO COMMAND");
 
 	}
 
 	@Override
 	public void visitSetPositionCommand(SetPositionCommand setPositionCommand) {
+		System.out.println("VISIT SET POSITION COMMAND");
 
+	}
+
+	public boolean getResult() {
+		return false;
 	}
 }
