c724095b6b23ce6417329d051ab1ac3a65397a17
DanielWap <daniel.waprzko@outlook.com>
2023-01-14 12:56:32 +0100

    Line Driver Adapter Changes
    Line Driver Adapter Changes

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDriverAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDriverAdapter.java
@@ -1,5 +1,7 @@
 package edu.kis.powp.jobs2d.drivers.adapter;
 
+import java.util.logging.Logger;
+
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.powp.jobs2d.Job2dDriver;
@@ -13,6 +15,8 @@ public class LineDriverAdapter implements Job2dDriver {
 	private String name;
 
 	private DrawPanelController drawController;
+	Logger logger = Logger.getLogger("global");
+	private static int totalDistance = 0;
 
 	public LineDriverAdapter(DrawPanelController drawController, ILine line, String name) {
 		super();
@@ -29,6 +33,8 @@ public class LineDriverAdapter implements Job2dDriver {
 
 	@Override
 	public void operateTo(int x, int y) {
+		int distance = (int) Math.sqrt(Math.pow(this.startX -x, 2)+Math.pow(this.startY -y, 2));
+		logger.info("total distance: " + (LineDriverAdapter.totalDistance += distance));
 		line.setStartCoordinates(this.startX, this.startY);
 		this.setPosition(x, y);
 		line.setEndCoordinates(x, y);
