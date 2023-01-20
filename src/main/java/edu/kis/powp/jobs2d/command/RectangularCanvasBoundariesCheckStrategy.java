c3c0c25e69f29d917febbb114767bb3ad411a8e8
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-01-20 15:19:54 +0100

    Fix bug in calculations

--- src/main/java/edu/kis/powp/jobs2d/command/RectangularCanvasBoundariesCheckStrategy.java
+++ src/main/java/edu/kis/powp/jobs2d/command/RectangularCanvasBoundariesCheckStrategy.java
@@ -5,6 +5,6 @@ import static java.lang.Math.abs;
 public class RectangularCanvasBoundariesCheckStrategy implements ICanvasBoundariesCheckStrategy {
 	@Override
 	public boolean checkExceedingBoundaries(ICanvas canvas, int X, int Y) {
-		return (abs(X) < canvas.getHeight() / 2) || (abs(Y) < canvas.getWidth() / 2);
+		return (abs(X) > canvas.getHeight() / 2) || (abs(Y) > canvas.getWidth() / 2);
 	}
 }
