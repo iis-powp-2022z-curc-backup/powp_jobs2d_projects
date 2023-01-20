7ac7c47e55b113105824c903118b9bef2926b5e2
DorianWlodarczyk <215365@edu.p.lodz.pl>
2023-01-20 19:47:11 +0100

    Got rid of unnecessary objects and simplified the code

--- src/main/java/edu/kis/powp/jobs2d/events/SelectTransformCommandOptionListener.java
+++ src/main/java/edu/kis/powp/jobs2d/events/SelectTransformCommandOptionListener.java
@@ -1,6 +1,7 @@
 package edu.kis.powp.jobs2d.events;
 
 import edu.kis.powp.jobs2d.command.transformers.ComplexTransformerCommand;
+import edu.kis.powp.jobs2d.command.transformers.TransformerCommand;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.drivers.adapter.TransformerDriver;
 
@@ -11,10 +12,10 @@ import java.awt.event.ActionListener;
 public class SelectTransformCommandOptionListener implements ActionListener {
 
     private final DriverManager driverManager;
-    private final ComplexTransformerCommand transformerCommands;
+    private final TransformerCommand transformerCommands;
     private final String name;
 
-    public SelectTransformCommandOptionListener(DriverManager driverManager, ComplexTransformerCommand commands, String name) {
+    public SelectTransformCommandOptionListener(DriverManager driverManager, TransformerCommand commands, String name) {
         this.driverManager = driverManager;
         this.transformerCommands = commands;
         this.name = name;
@@ -22,6 +23,6 @@ public class SelectTransformCommandOptionListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        driverManager.setCurrentDriver(new TransformerDriver(driverManager.getCurrentDriver(), (ComplexTransformerCommand) transformerCommands, name));
+        driverManager.setCurrentDriver(new TransformerDriver(driverManager.getCurrentDriver(), transformerCommands, name));
     }
 }
