262e9084f6b9ed553ed9a3c0aebaf4799afaabdf
kamilmichalskiya <kamilmichalskiya@users.noreply.github.com>
2023-01-21 13:05:40 +0100

    Extract feature listener class

--- src/main/java/edu/kis/powp/jobs2d/features/MainFeature.java
+++ src/main/java/edu/kis/powp/jobs2d/features/MainFeature.java
@@ -1,27 +1,38 @@
 package edu.kis.powp.jobs2d.features;
 
 import edu.kis.powp.appbase.Application;
+import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.LoggerDriver;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainFeature {
-	public static boolean loggerActive = false;
-	private static final LoggerDriver loggerDriver = new LoggerDriver();
-
 	public static void setupFeaturePlugin(Application application) {
 		application.addComponentMenu(MainFeature.class, "Feature");
-		application.addComponentMenuElementWithCheckBox(MainFeature.class, "Logger", getLoggerActionListener(), loggerActive);
+		addFeatureCheckBox(application, "Logger", new LoggerDriver());
 	}
 
-	private static ActionListener getLoggerActionListener() {
-		return e -> {
-			loggerActive = !loggerActive;
-			if (loggerActive) {
-				DriverFeature.getDriverManager().addFeatureDriver(loggerDriver);
+	private static void addFeatureCheckBox(Application application, String label, LoggerDriver driver) {
+		application.addComponentMenuElementWithCheckBox(MainFeature.class, label, new FeatureListener(driver), false);
+	}
+
+	static class FeatureListener implements ActionListener {
+		private boolean featureActive = false;
+		private final Job2dDriver driver;
+
+		public FeatureListener(Job2dDriver driver) {
+			this.driver = driver;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			featureActive = !featureActive;
+			if (featureActive) {
+				DriverFeature.getDriverManager().addFeatureDriver(driver);
 			} else {
-				DriverFeature.getDriverManager().removeFeatureDriver(loggerDriver);
+				DriverFeature.getDriverManager().removeFeatureDriver(driver);
 			}
-		};
+		}
 	}
 }
