6394bca6364820c840ec84035fce763fbb0670d8
Czarek <keracz1997@gmail.com>
2023-01-21 12:30:00 +0100

    change VisitableDriver

--- src/main/java/edu/kis/powp/jobs2d/drivers/VisitableDriver.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/VisitableDriver.java
@@ -2,10 +2,6 @@ package edu.kis.powp.jobs2d.drivers;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
 
-public interface SingleDriver extends Cloneable {
-
-	public void execute(Job2dDriver driver);
-	public Object clone();
-
+public interface SingleDriver extends Job2dDriver {
 	public void accept(Visitor visitor);
 }
