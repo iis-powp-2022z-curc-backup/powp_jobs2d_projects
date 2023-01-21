33de3d662b5245fec563de5c2d53678d4caae28e
kamilmichalskiya <kamilmichalskiya@users.noreply.github.com>
2023-01-21 12:46:28 +0100

    Fix after pr review

--- src/main/java/edu/kis/powp/jobs2d/drivers/DriverManager.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/DriverManager.java
@@ -3,23 +3,22 @@ package edu.kis.powp.jobs2d.drivers;
 import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.LoggerDriver;
 import edu.kis.powp.jobs2d.drivers.composite.DriverComposite;
-import edu.kis.powp.jobs2d.features.MainFeature;
 import edu.kis.powp.observer.Publisher;
 
 /**
- * Driver manager provides means to setup the driver. It also enables other
- * components and features of the application to react on configuration changes.
+ * Driver manager provides means to setup the driver. It also enables other components and features of the application to react on
+ * configuration changes.
  */
 public class DriverManager {
 
 	private Job2dDriver currentDriver = new LoggerDriver();
 	private final Publisher changePublisher = new Publisher();
+	private final DriverComposite featureDrivers = new DriverComposite();
 
 	/**
 	 * @param driver Set the driver as current.
 	 */
 	public synchronized void setCurrentDriver(Job2dDriver driver) {
-
 		currentDriver = driver;
 		changePublisher.notifyObservers();
 	}
@@ -28,14 +27,18 @@ public class DriverManager {
 	 * @return Current driver.
 	 */
 	public synchronized Job2dDriver getCurrentDriver() {
-		if (MainFeature.loggerActive) {
-			final DriverComposite driverComposite = new DriverComposite();
-			driverComposite.add(currentDriver);
-			driverComposite.add(new LoggerDriver());
-			return driverComposite;
-		} else {
-			return currentDriver;
-		}
+		final DriverComposite driverComposite = new DriverComposite();
+		driverComposite.add(currentDriver);
+		driverComposite.add(featureDrivers);
+		return driverComposite;
+	}
+
+	public synchronized void addFeatureDriver(Job2dDriver driver) {
+		featureDrivers.add(driver);
+	}
+
+	public synchronized void removeFeatureDriver(Job2dDriver driver) {
+		featureDrivers.remove(driver);
 	}
 
 	public Publisher getChangePublisher() {
--- src/main/java/edu/kis/powp/jobs2d/features/MainFeature.java
+++ src/main/java/edu/kis/powp/jobs2d/features/MainFeature.java
@@ -1,13 +1,27 @@
 package edu.kis.powp.jobs2d.features;
 
 import edu.kis.powp.appbase.Application;
+import edu.kis.powp.jobs2d.LoggerDriver;
+
+import java.awt.event.ActionListener;
 
 public class MainFeature {
 	public static boolean loggerActive = false;
+	private static final LoggerDriver loggerDriver = new LoggerDriver();
 
 	public static void setupFeaturePlugin(Application application) {
 		application.addComponentMenu(MainFeature.class, "Feature");
-		application.addComponentMenuElementWithCheckBox(MainFeature.class, "Logger", e -> loggerActive = !loggerActive,
-				loggerActive);
+		application.addComponentMenuElementWithCheckBox(MainFeature.class, "Logger", getLoggerActionListener(), loggerActive);
+	}
+
+	private static ActionListener getLoggerActionListener() {
+		return e -> {
+			loggerActive = !loggerActive;
+			if (loggerActive) {
+				DriverFeature.getDriverManager().addFeatureDriver(loggerDriver);
+			} else {
+				DriverFeature.getDriverManager().removeFeatureDriver(loggerDriver);
+			}
+		};
 	}
 }
