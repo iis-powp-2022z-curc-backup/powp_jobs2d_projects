0d60f46180a7532a436119c12e215247912cfa91
Czarek <keracz1997@gmail.com>
2023-01-21 12:31:29 +0100

    rename SingleDriver into VisitableDriver

--- src/main/java/edu/kis/powp/jobs2d/drivers/VisitableDriver.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/VisitableDriver.java
@@ -2,6 +2,6 @@ package edu.kis.powp.jobs2d.drivers;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
 
-public interface SingleDriver extends Job2dDriver {
+public interface VisitableDriver extends Job2dDriver {
 	public void accept(Visitor visitor);
 }
