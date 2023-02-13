f3e8836d06dbf2bd7ab4ede629694eb66c7315b5
Czarek Karczewski <keracz1997@gmail.com>
2023-02-13 19:51:08 +0100

    add SubscribeDriversCounterVisitor

--- src/test/java/edu/kis/powp/jobs2d/drivers/SubscribeDriversCounterVisitor.java
+++ src/test/java/edu/kis/powp/jobs2d/drivers/SubscribeDriversCounterVisitor.java
@@ -1,4 +1,27 @@
 package edu.kis.powp.jobs2d.drivers;
 
-public class SubscribeDriversCounterVisitor {
+
+import edu.kis.powp.observer.Subscriber;
+
+import java.util.logging.Logger;
+
+public class SubscribeDriversCounterVisitor implements Subscriber {
+	private VisitableDriver visitableDriver;
+	private DriverManager manager;
+	private Logger logger = Logger.getLogger("global");
+	private DriverCounterVisitor driverCounterVisitor;
+	public SubscribeDriversCounterVisitor(DriverCounterVisitor driverCounterVisitor, DriverManager driverManager, VisitableDriver visitableDriver) {
+		this.driverCounterVisitor = driverCounterVisitor;
+		this.manager = driverManager;
+		this.visitableDriver = visitableDriver;
+	}
+
+	public void update() {
+		visitableDriver = manager.getCurrentDriver();
+		visitableDriver.accept(driverCounterVisitor);
+
+		this.logger.info("driver composite wywołano " + driverCounterVisitor.getDriverComposite()  + " razy");
+		this.logger.info("command recorder wywołano " + driverCounterVisitor.getCommandsRecorderDriver()  + " razy");
+		this.logger.info("line driver wywołano " + driverCounterVisitor.getLineDriverAdapter()  + " razy");
+	}
 }
