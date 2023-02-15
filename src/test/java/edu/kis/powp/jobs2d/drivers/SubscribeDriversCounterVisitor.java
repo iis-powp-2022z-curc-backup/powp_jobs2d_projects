8e90ceff386ee53b882131ccec1e36d07c2f9530
Czarek Karczewski <keracz1997@gmail.com>
2023-02-15 11:23:37 +0100

    fix Subscriber

--- src/test/java/edu/kis/powp/jobs2d/drivers/SubscribeDriversCounterVisitor.java
+++ src/test/java/edu/kis/powp/jobs2d/drivers/SubscribeDriversCounterVisitor.java
@@ -7,19 +7,21 @@ import java.util.logging.Logger;
 
 public class SubscribeDriversCounterVisitor implements Subscriber {
 	private VisitableDriver visitableDriver;
+	private DriverManager manager;
 	private Logger logger = Logger.getLogger("global");
 	private DriverCounterVisitor driverCounterVisitor;
-	public SubscribeDriversCounterVisitor(DriverCounterVisitor driverCounterVisitor, VisitableDriver visitableDriver) {
+	public SubscribeDriversCounterVisitor(DriverCounterVisitor driverCounterVisitor, DriverManager driverManager, VisitableDriver visitableDriver) {
 		this.driverCounterVisitor = driverCounterVisitor;
 		this.visitableDriver = visitableDriver;
+		this.manager = driverManager;
 	}
 
 	public void update() {
+		manager.getCurrentDriver().accept(driverCounterVisitor);
 
-		visitableDriver.accept(driverCounterVisitor);
-
-		this.logger.info("driver composite wywołano " + driverCounterVisitor.getDriverComposite()  + " razy");
-		this.logger.info("command recorder wywołano " + driverCounterVisitor.getCommandsRecorderDriver()  + " razy");
-		this.logger.info("line driver wywołano " + driverCounterVisitor.getLineDriverAdapter()  + " razy");
+		this.logger.info("driver composite wywołano: " + driverCounterVisitor.getDriverComposite()  + " razy");
+		this.logger.info("line driver wywołano: " + driverCounterVisitor.getLineDriverAdapter()  + " razy");
+		this.logger.info("loger driver wywołano: " + driverCounterVisitor.getLoggerDriverDecorator()  + " razy");
+		driverCounterVisitor.reset();
 	}
 }
