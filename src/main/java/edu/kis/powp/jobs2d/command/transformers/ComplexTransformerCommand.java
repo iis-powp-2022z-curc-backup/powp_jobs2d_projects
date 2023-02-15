f5a69a77dead23585af776622b1005aad5c30d02
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-02-15 17:46:29 +0100

    Update ComplexTransformerCommand.java

--- src/main/java/edu/kis/powp/jobs2d/command/transformers/ComplexTransformerCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/transformers/ComplexTransformerCommand.java
@@ -1,10 +1,11 @@
 package edu.kis.powp.jobs2d.command.transformers;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 public class ComplexTransformerCommand implements TransformerCommand {
     private final List<TransformerCommand> transformerCommands;
-
+    private Logger logger = Logger.getLogger("global");
     public ComplexTransformerCommand(List<TransformerCommand> transformerCommands) {
         this.transformerCommands = transformerCommands;
     }
@@ -12,8 +13,19 @@ public class ComplexTransformerCommand implements TransformerCommand {
     @Override
     public TransformedCoords execute(TransformedCoords coords) {
         TransformedCoords transformedCoords = coords;
-        for (TransformerCommand command : transformerCommands)
+        ComplexTransformerVisitor visitor = new ComplexTransformerVisitor();
+        for (TransformerCommand command : transformerCommands){
             transformedCoords = command.execute(transformedCoords);
+            command.accept(visitor);
+        }
+
+        this.logger.info("Rotacje wykonano " + visitor.getCounterRotateCommand()  + " razy");
+        this.logger.info("Przeniesienie wykonano " + visitor.getCounterTranslateCommand()  + " razy");
+        this.logger.info("Skalowanie " + visitor.getCounterScaleCommand()  + " razy");
+
         return transformedCoords;
     }
+
+    @Override
+    public void accept(TransformerVisitor visitor) {}
 }
